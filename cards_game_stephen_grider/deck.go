d19e92ba00989fb6a532034892ba70f9ed0f7611
Dari <dari.k@abv.bg>
2022-02-07 15:26:03 +0200

    newDeck function

--- cards_game_stephen_grider/deck.go
+++ cards_game_stephen_grider/deck.go
@@ -6,9 +6,24 @@ import "fmt"
 
 type deck []string //extends slice of string
 
-// new receiver func of deck type
+// new receiver func of deck type - d is like self
+// By convention we refer to receiver with 1 or 2 letters from the type
 func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
 	}
+}
+
+func newDeck() deck {
+	cards := deck {}
+	cardSuites := []string {"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardsValues := []string {"Ace", "Two", "Three", "Four"}
+	
+	for _, cardSuite := range cardSuites {
+		for _, cardValue := range cardsValues {
+			newCard := cardValue + " of " + cardSuite
+			cards = append(cards, newCard)
+		}
+	}
+	return cards
 }
--- cards_game_stephen_grider/main.go
+++ cards_game_stephen_grider/main.go
@@ -1,14 +1,14 @@
 package main
 
 func main() {
-	cards := deck {"Ace of Spades", newCard()}
-	card := newCard()
+	// cards := deck {"Ace of Spades", newCard()}
+	// card := newCard()
 	// append does not modifies the existing slice but returns a new one
-	cards = append(cards, card)
-	
+	// cards = append(cards, card)
+	cards := newDeck()
 	cards.print()
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
