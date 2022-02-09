92c71b2a2f60d12c1d2d774dc1655200395b4e90
dkutelov <dkutelov@digitals.bg>
2022-02-09 10:05:18 +0200

    Arrays demo

--- zerotomastery.io-golang/src/lectures/demo/arrays/arrays.go
+++ zerotomastery.io-golang/src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name string
+	cleaned bool
+}
 
+
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i] // always make a copy of interator variable
+
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room {
+		{name: "Office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+
+	fmt.Println("Preforming cleaning")
+
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanliness(rooms)
 }
