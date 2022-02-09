13cb3c6dbcf3303216d41d262cb1854965f82591
dkutelov <dkutelov@digitals.bg>
2022-02-09 19:26:17 +0200

    slices exercise

--- zerotomastery.io-golang/src/lectures/exercise/slices/slices.go
+++ zerotomastery.io-golang/src/lectures/exercise/slices/slices.go
@@ -18,5 +18,26 @@ import "fmt"
 
 type Part string
 
+func printStats(line []Part) {
+	for i := 0; i < len(line); i++ {
+		part := line[i]
+		fmt.Println(part)
+	}
+}
+
 func main() {
+	assemblyLine := make([]Part, 3)
+	assemblyLine[0] = "Pipe"
+	assemblyLine[1] = "Bolt"
+	assemblyLine[2] = "Sheet"
+
+	fmt.Println("3 parts:")
+	printStats(assemblyLine)
+
+	assemblyLine = append(assemblyLine, "Washer", "Wheel")
+	fmt.Println("\nAdded 2 parts:")
+	printStats(assemblyLine)
+	assemblyLine = assemblyLine[3:]
+	fmt.Println("\nSliced:")
+	printStats(assemblyLine)
 }
