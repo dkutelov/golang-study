cfed2dea9e84ab32a6a9b2308000c0af79a9bb2f
dkutelov <dkutelov@digitals.bg>
2022-02-09 18:50:41 +0200

    Arrays exercise

--- zerotomastery.io-golang/src/lectures/exercise/arrays/arrays.go
+++ zerotomastery.io-golang/src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,43 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name string
+	price float64
+}
 
+
+func getListInfo(shoppingList [4]Product) {
+	productCount := 0
+	lastProduct := Product{}
+	var totalCost float64
+
+	for i := 0; i < len(shoppingList); i++ {
+		currentProduct := shoppingList[i]
+
+		if currentProduct != (Product{}) {
+			productCount += 1
+			lastProduct = currentProduct
+			totalCost += currentProduct.price
+		}
+	}
+
+	fmt.Println("The last item on the list is", lastProduct)
+	fmt.Println("The total number of items is", productCount)
+	fmt.Println("The total cost of the products is", totalCost)
+}
+
+
+func main() {
+	shoppingList := [4]Product {
+		{name: "item1", price: 1.5},
+		{name: "item2", price: 2.5},
+		{name: "item3", price: 3.5},
+	}
+
+	getListInfo(shoppingList)
+	
+	shoppingList[3] = Product{name: "item 4", price: 4.5}
+
+	getListInfo(shoppingList)
 }
