dbc0a9d3ea8b5e73f7901b7e43ee61a9e27606b1
Dari <dari.k@abv.bg>
2022-02-10 14:38:23 +0200

    Demo pointers

--- zerotomastery.io-golang/src/lectures/demo/pointers/pointers.go
+++ zerotomastery.io-golang/src/lectures/demo/pointers/pointers.go
@@ -2,6 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
 
+type Counter struct {
+	hits int
+}
+
+// takes a pointer to a Counter
+func increment(counter *Counter) {
+	counter.hits += 1 // structures is placing *counter
+	fmt.Println("Counter", counter)
+}
+
+func replace(old *string, new string, counter *Counter) {
+	*old = new
+	increment(counter)
+}
+
+func main() {
+	// Function values are passed by value -> many compies
+	// value := 10
+	// var valuePtr *int
+	// valuePtr = &value
+
+	counter := Counter{}
+
+	hello := "Hello"
+	world := "World"
+	fmt.Println(hello, world)
+
+	replace(&hello, "Hi", &counter)
+	fmt.Println(hello, world)
+
+	phrase := []string{hello, world}
+
+	replace(&phrase[1], "Go!", &counter)
+	fmt.Println(phrase)
 }
