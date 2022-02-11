077f040dd344f3be6cd6633cf61817f2aabc06d8
dkutelov <dkutelov@digitals.bg>
2022-02-11 17:29:34 +0200

    Demo interfaces

--- zerotomastery.io-golang/src/lectures/demo/interfaces/interfaces.go
+++ zerotomastery.io-golang/src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
-func main() {
+//By convention name ends on -er
+type Preparer interface {
+	PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("Cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("Chop salad")
+	fmt.Println("add salad")
+}
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+
+	for _, dish := range dishes {
+		fmt.Printf("--Dish %v--\n", dish)
+		dish.PrepareDish()
+	}
+}
+
+func main() {
+	dishes := []Preparer {
+		Chicken("Chicken wings"),
+		Salad("Nicoise"),
+	}
+
+	prepareDishes(dishes)
 }
