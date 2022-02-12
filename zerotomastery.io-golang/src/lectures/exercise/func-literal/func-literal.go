00bebb11e87c48d53d7439ab5a296041ffe73a59
dkutelov <dkutelov@digitals.bg>
2022-02-12 16:44:26 +0200

    Exercise function literals

--- zerotomastery.io-golang/src/lectures/exercise/func-literal/func-literal.go
+++ zerotomastery.io-golang/src/lectures/exercise/func-literal/func-literal.go
@@ -18,7 +18,18 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
+
+type LineCallback func(line string)
+
+func lineIterator(lines []string, callback LineCallback) {
+	for _, line := range lines {
+		callback(line)
+	}
+}
 
 func main() {
 	lines := []string{
@@ -28,4 +39,31 @@ func main() {
 		"12 spaces,",
 		"and 4 punctuation marks in these lines of text!",
 	}
+
+	letters := 0
+	digits := 0
+	spaces := 0
+	punctuation := 0
+
+	lineFunc := func(text string) {
+		for _, r := range text {
+			if unicode.IsLetter(r) {
+				letters += 1
+			} else if unicode.IsDigit(r) {
+				digits += 1
+			} else if unicode.IsSpace(r) {
+				spaces += 1
+			} else if unicode.IsPunct(r) {
+				punctuation += 1
+			}
+		}
+	}
+
+	lineIterator(lines, lineFunc)
+
+	fmt.Printf("The text contains: %v letter, %v digits, %v spaces, %v punctuations.\n", 
+	letters, 
+	digits, 
+	spaces,
+	punctuation)
 }
