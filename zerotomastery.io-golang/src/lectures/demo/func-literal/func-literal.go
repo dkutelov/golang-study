b1060080e0d0a00361269b48329ade464b157188
dkutelov <dkutelov@digitals.bg>
2022-02-12 16:20:24 +0200

    Demo function literals

--- zerotomastery.io-golang/src/lectures/demo/func-literal/func-literal.go
+++ zerotomastery.io-golang/src/lectures/demo/func-literal/func-literal.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+type Operation func(lhs, rhs int) int
+
+func compute(lhs, rhs int, op Operation) int {
+	fmt.Printf("Running a computation with %v and %v.\n", lhs, rhs)
+	return op(lhs, rhs)
+}
 
 func main() {
+	fmt.Println("2 + 2 =", compute(2,2,add))
 
+	fmt.Println("10 - 2 =", compute(10,2,func(lhs, rhs int) int {
+		return lhs - rhs
+	}))
+
+	mul := func(lhs, rhs int) int {
+		fmt.Printf("Multiplying %v * %v = ", lhs, rhs)
+		return lhs * rhs
+	}
+
+	fmt.Println(compute(5,5,mul))
 }
