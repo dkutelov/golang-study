e5cd50d774772dc2230c0394a89c23c5b01ae288
dkutelov <dkutelov@digitals.bg>
2022-02-12 10:41:29 +0200

    Simple error demo

--- zerotomastery.io-golang/src/lectures/demo/errors/errors.go
+++ zerotomastery.io-golang/src/lectures/demo/errors/errors.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func main() {
+type Stuff struct {
+	values []int
+}
 
+func (s Stuff) Get(index int) (int, error) {
+	if index > len(s.values) {
+		return 0, errors.New(fmt.Sprintf("No element at index %v", index))
+	} else {
+		return s.values[index], nil
+	}
+}
+
+func main() {
+	stuff := Stuff{}
+	// stuff.values = append(stuff.values, 5)
+	value, err := stuff.Get(1)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(value)
+	}
+
+	
+	fmt.Println(stuff)
 }
