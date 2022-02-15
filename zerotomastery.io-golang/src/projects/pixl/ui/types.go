91e6f041a0de51c4e9c79677c99409552cdc9d2d
dkutelov <dkutelov@digitals.bg>
2022-02-15 10:30:55 +0200

    Pixl - container

---
+++ zerotomastery.io-golang/src/projects/pixl/ui/types.go
@@ -0,0 +1,15 @@
+package ui
+
+import (
+	"pixl/apptype"
+	"pixl/swatch"
+
+	"fyne.io/fyne/v2"
+)
+
+
+type AppInit struct {
+	PixlWindow fyne.Window
+	State *apptype.State
+	Swatches []*swatch.Swatch
+}
