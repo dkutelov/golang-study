9222599a0714b4ef7e69d8a3a1c695e788fa6068
dkutelov <dkutelov@digitals.bg>
2022-02-15 18:00:31 +0200

    color picker

--- zerotomastery.io-golang/src/projects/pixl/ui/layout.go
+++ zerotomastery.io-golang/src/projects/pixl/ui/layout.go
@@ -1,7 +1,13 @@
 package ui
 
+import "fyne.io/fyne/v2/container"
+
 func Setup(app *AppInit) {
 	swatchesContainer := BuidSwatches(app)
 
-	app.PixlWindow.SetContent(swatchesContainer)
+	colorPicker := SetupColorPicker(app)
+
+	appLayout := container.NewBorder(nil, swatchesContainer, nil, colorPicker)
+
+	app.PixlWindow.SetContent(appLayout)
 }
